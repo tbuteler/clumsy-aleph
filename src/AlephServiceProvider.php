7f259b1a8b7da1f45898c6fd38c3693016d397af
Tomas Buteler <tomas.buteler@wiz.pt>
2015-11-15 17:26:59 +0000

    Upgraded to Laravel 5; added config and option to send log entry to endpoint

---
+++ src/AlephServiceProvider.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Clumsy\Aleph;
+
+use Exception;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Request;
+
+class AlephServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    protected $endpoint;
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__.'/config.php', 'clumsy.aleph');
+
+        $this->endpoint = config('clumsy.aleph.endpoint');
+
+        $this->app['log']->getMonolog()->pushProcessor(function ($record) {
+
+            try {
+
+                $request = $this->app['request'];
+                $requestInfo = [
+                    'ip'      => $request->ip(),
+                    'url'     => $request->fullUrl(),
+                    'method'  => $request->method(),
+                    'isAjax'  => $request->ajax(),
+                    'agent'   => $request->header('USER_AGENT'),
+                    'input'   => $request->all(),
+                    'cookies' => $request->cookie(),
+                    'user'    => $request->user(),
+                    'session' => [
+                        'started'    => $request->session()->isStarted(),
+                        'id'         => $request->session()->getId(),
+                        'attributes' => $request->session()->all(),
+                        'handler'    => class_basename($request->session()->getHandler()),
+                    ],
+                ];
+
+                if (!is_null($this->endpoint)) {
+                    $record['request'] = $requestInfo;
+                    $this->postRecord($record);
+                }
+
+                $record['message'] = json_encode($requestInfo, true).' |[o]| '.$record['message'];
+
+                return $record;
+
+            } catch (Exception $e) {
+
+                return $record;
+            }
+        });
+    }
+
+    public function postRecord(array $data)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $this->endpoint);
+        curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+        curl_setopt($ch, CURLOPT_ENCODING, 'UTF-8');
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data, true));
+        curl_exec($ch);
+        curl_close($ch);
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
+}
